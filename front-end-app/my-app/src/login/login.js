47fb7d5bbe46c753a90f30a7c23a77a209898550
mohanapriya2174 <132121558+mohanapriya2174@users.noreply.github.com>
2025-08-25 23:45:33 +0530

    Update login.js

--- front-end-app/my-app/src/login/login.js
+++ front-end-app/my-app/src/login/login.js
@@ -11,7 +11,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:4000/api/login", {
+    const response = await fetch("https://employee-management-system-zqqv.onrender.com/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
